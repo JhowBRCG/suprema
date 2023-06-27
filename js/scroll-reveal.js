24cb31880f7c2e77fae4ba5fd3dbc74429ac2399
Jhonathan <guarino90@gmail.com>
2023-06-26 22:49:49 -0300

    add scrollReveal tech

---
+++ js/scroll-reveal.js
@@ -0,0 +1,48 @@
+// ScrollReveal().reveal(".hero", { delay: 400 });
+
+//Service__card //
+
+ScrollReveal().reveal(".service__card.data", {
+  delay: 200,
+  origin: "top",
+  distance: "50px",
+});
+
+ScrollReveal().reveal(".service__card.socials", {
+  delay: 400,
+  origin: "top",
+  distance: "50px",
+});
+
+ScrollReveal().reveal(".service__card.page", {
+  delay: 600,
+  origin: "top",
+  distance: "50px",
+});
+
+// Card-review //
+
+ScrollReveal().reveal(".card-review.one", {
+  delay: 200,
+  origin: "bottom",
+  distance: "50px",
+});
+ScrollReveal().reveal(".card-review.two", {
+  delay: 400,
+  origin: "bottom",
+  distance: "50px",
+});
+
+ScrollReveal().reveal(".card-review.three", {
+  delay: 600,
+  origin: "bottom",
+  distance: "50px",
+});
+
+// About__img //
+
+ScrollReveal().reveal(".about__img", {
+  delay: 400,
+  origin: "right",
+  distance: "100px",
+});
